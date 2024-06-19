10b584ef313d1f37abb5750f95d7daff55ab4898
Murage-Ivy <ivymurage2000@gmail.com>
2024-06-19 15:14:34 +0200

    add error handling

--- fetchVehiclesDetails.js
+++ fetchVehiclesDetails.js
@@ -1,16 +1,25 @@
 const getVehiclesDetails = async () => {
-  const response = await fetch(
-    `https://vpic.nhtsa.dot.gov/api/vehicles/GetAllManufacturers?format=json&page=2`
-  );
+  try {
+    const response = await fetch(
+      `https://vpic.nhtsa.dot.gov/api/vehicles/GetAllManufacturers?format=json&page=2`
+    );
 
-  if (!response.ok) {
-    throw new Error("Failed to get vehicle details");
+    if (!response.ok) {
+      throw new Error("Failed to get vehicle details");
+    }
+    const { Results } = await response.json();
+
+    if (!Results) {
+      throw new Error("No vehicle details");
+    }
+
+    let usa = Results.filter(
+      ({ Country }) => Country === "UNITED STATES (USA)"
+    ).map(({ Mfr_Name }) => Mfr_Name);
+    console.log(usa);
+  } catch (err) {
+    console.error(err.message);
   }
-  const { Results } = await response.json();
-  let usa = Results.filter(
-    ({ Country }) => Country === "UNITED STATES (USA)"
-  ).map(({ Mfr_Name }) => Mfr_Name);
-  console.log(usa);
 };
 
 getVehiclesDetails();
